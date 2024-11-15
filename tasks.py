efcab10eeef6d03a68c180ed2726858ba3b805a4
CharlotteGroot <charlottegroot02@gmail.com>
2024-11-15 15:39:11 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -81,7 +81,13 @@ def compute_output_size_2d(input_matrix, kernel_matrix):
 def convolute_2d(input_matrix, kernel_matrix):
     # Tip: same tips as above, but you might need a nested loop here in order to
     # define which parts of the input matrix need to be multiplied with the kernel matrix.
-    pass
-
+    output_height, output_width = compute_output_size_2d(input_matrix, kernel_matrix)
+    output_matrix = np.zeros((output_height, output_width))
+    kernel_height, kernel_width = kernel_matrix.shape
+    for i in range(output_height):
+        for j in range(output_width):
+            sub_matrix = input_matrix[i:i+kernel_height, j:j+kernel_width]
+            output_matrix[i, j] = np.sum(sub_matrix * kernel_matrix)
+    return output_matrix
 
 # -----------------------------------------------
